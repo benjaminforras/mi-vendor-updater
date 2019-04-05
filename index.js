73d670bb0b8dd87ef43af04e87aaf02ccaf37852
TryHardDood <rsnconfigs@gmail.com>
2019-04-05 11:32:03 +0000

    Fix file upload

--- index.js
+++ index.js
@@ -147,7 +147,8 @@ let main = async () => {
                     }
 
                     try {
-                        await octokit.repos.uploadReleaseAsset({ headers: { "content-length": fs.statSync(file).size, "content-type": "application/octet-stream" }, url: result.data.upload_url, name: file, label: file, file: file });
+                        const size = fs.readFileSync(file);
+                        await octokit.repos.uploadReleaseAsset({ headers: { "content-length": size.length, "content-type": "application/octet-stream" }, url: result.data.upload_url, name: file, label: file, file: file });
                         console.log(file + ' uploaded');
                     } catch (e) {
                         console.log('Couldn\'t upload asset');
